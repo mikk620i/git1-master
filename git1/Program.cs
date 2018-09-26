2e094e910d7796deeefc12b05d7eb5659b014c42
Mikkel Jørgen Bang <mikk620i@edu.eal.dk>
2018-09-26 09:34:08 +0200

    ændret pc 1

--- git1/Program.cs
+++ git1/Program.cs
@@ -10,8 +10,8 @@ namespace git1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git1 ændret på pc2");
-            Console.WriteLine("ny ændring pc 2");
+            Console.WriteLine("hello git1 ændret på pc2");
+            Console.WriteLine("Ny Ændring pc 2");
            
             Console.ReadLine();
         }
