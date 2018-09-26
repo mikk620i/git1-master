0618b08c1062c04a8adccb960bdd173b746ed550
Mikkel Jørgen Bang <mikk620i@edu.eal.dk>
2018-09-26 08:54:08 +0200

    Gedeost

---
+++ git1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git1 ændret på pc2");
+            Console.WriteLine("ny ændring pc 2");
+            Console.WriteLine("test konfligt pc2");
+            Console.ReadLine();
+        }
+    }
+}
